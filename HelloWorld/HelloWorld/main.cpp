a88602486b164b35aff44be349d87c9469d5617a
최지욱 <86818323+choijiuk1017@users.noreply.github.com>
2022-11-21 17:51:42 +0900

    11/21 수업 자료

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -12,6 +12,57 @@
 
 using namespace std;
 
+class Student
+{
+	string name;
+	int age;
+
+public:
+	Student(const string& name, int age) : name(name), age(age) {}
+
+	//전역 함수임, 멤버 함수 아님
+	//전역 함수의 내용을 클래스 안에 구현해 놓은 것
+	friend ostream& operator << (ostream& os, const Student& s);
+	/*{
+		os << "name =" << s.name << ", age = " << s.age << endl;
+		return os;
+	}*/
+	friend template<typename T>
+	friend bool isMultipleOfFive<Student>(const Student& s);
+};
+
+ostream& operator << (ostream& os, const Student& s)
+{
+	os << "name =" << s.name << ", age = " << s.age << endl;
+	return os;
+}
+
+
+template<typename T>
+ostream & operator << (ostream & os, const vector<T>& container)
+{
+	for (auto &item : container)
+	{
+		//data[i].print();
+		cout << item << endl;
+		//(cout.operator <<(data[i])).operator(endl);
+		//operator<<(operator << (cout, data[i]), endl);
+	}
+	return os;
+}
+
+template<typename T>
+bool isMultipleOfFive(const T& item)
+{
+	return item % 5 == 0;
+}
+
+template<>
+bool isMultipleOfFive(const Student& item)
+{
+	return item.age % 5 = 0;
+}
+
 int main()
 {
 	//vector<int> ints(10); //10 만큼의 배열 공간 생성, 10 대입 아님
@@ -25,53 +76,76 @@ int main()
 
 	//vector<int> ints = { 20, 30, 50, 1, 24 };
 
-	vector<int> ints{ 20, 30, 50, 1, 24 };
+	//vector<int> ints{ 50, 20, 100, 50, 24 };
+	vector<Student> students{ {"Beomjoo Seo", 18} ,{"Mr. Present", 60}, {"Mr. Lee", 62}, {"Mr.Moon", 60} };
 
-	for (/*vector<int>::const_iterator*/ auto it = ints.cbegin(); it != ints.cend(); it++)
+	/*
+	for (vector<int>::const_iterator auto it = ints.cbegin(); it != ints.cend(); it++)
 	{
 		//cbegin 읽기 전용, begin 고치기 가능
 		auto item = *it;
 
 		cout << "item = " << item << endl;
 	}
+	*/
+
+	cout << students << endl;
+	//print(students.data(), students.size());
 	
-	for (/*vector<int>::iterator*/ auto it = ints.begin(); it != ints.end(); it++)
-	{
-		//cbegin 읽기 전용, begin 고치기 가능
-		auto& item = *it;
+	//for (/*vector<int>::iterator*/ auto it = ints.begin(); it != ints.end(); it++)
+	//{
+	//	//cbegin 읽기 전용, begin 고치기 가능
+	//	auto& item = *it;
 
-		item++;
+	//	item++;
 
-		cout << "item = " << item << endl;
-	}
+	//	cout << "item = " << item << endl;
+	//}
 
-	//C#의 foreach문과 동일, ranged for문
-	for (auto item : ints)
-	{
-		item++;
-		cout << "item = " << item << endl;
-	}
+	////C#의 foreach문과 동일, ranged for문
+	//for (auto item : ints)
+	//{
+	//	item++;
+	//	cout << "item = " << item << endl;
+	//}
 
-	for (auto item : ints)
-	{
-		cout << "item = " << item << endl;
-	}
-	//item이 바뀌긴 하나 원본에게 영향을 끼치진 않음
+	//for (auto item : ints)
+	//{
+	//	cout << "item = " << item << endl;
+	//}
+	////item이 바뀌긴 하나 원본에게 영향을 끼치진 않음
 
-	for (auto& item : ints)
-	{
-		item++;
-		cout << "item = " << item << endl;
-	}
+	//for (auto& item : ints)
+	//{
+	//	item++;
+	//	cout << "item = " << item << endl;
+	//}
 
-	for (auto item : ints)
-	{
-		cout << "item = " << item << endl;
-	}
+	//for (auto item : ints)
+	//{
+	//	cout << "item = " << item << endl;
+	//}
 	//item도 바뀌고 원본도 바뀜
 
 	//장점: 깔끔함, 인덱스 오류가 발생할 일 없음
 	//단점: 새로운 배열 추가, 삭제 불가능
+
+	cout << endl << "find" << endl;
+
+	/*vector<int> ::iterator*/ auto pos = students.begin();
+
+	/*while ((pos = find(pos, ints.end(), 50)) != ints.end())
+	{
+		cout << *pos << endl;
+		pos++;
+	}*/
+
+	while ((pos = find_if(pos, students.end(), isMultipleOfFive<Student>)) != students.end())
+	{
+		//cout << *pos << endl;
+		pos++;
+
+	}
 
 	return 0;
 
